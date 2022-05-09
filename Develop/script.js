e5d0eeadc381db0bb874c13b3f0ac813c23531c2
djpdim <dimospagkas@yahoo.gr>
2022-05-08 21:20:30 -0400

    update code

--- Develop/script.js
+++ Develop/script.js
@@ -1,111 +1,173 @@
 // Assignment code here
-var enter;
-var confirmNumber;
-var confirmCharacter;
-var confirmUppercase;
-var confirmLowercase;
-
+var enter
+var confirmNumber
+var confirmCharacter
+var confirmUppercase
+var confirmLowercase
 
 // Choise objects
-character = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
-number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
+character = [
+    "@",
+    "%",
+    "+",
+    "\\",
+    "/",
+    "'",
+    "!",
+    "#",
+    "$",
+    "^",
+    "?",
+    ":",
+    ",",
+    ")",
+    "(",
+    "}",
+    "{",
+    "]",
+    "[",
+    "~",
+    "-",
+    "_",
+    ".",
+]
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+lowercase = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+]
+uppercase = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+]
 
 // Choices declared outside the if statement
 
-var get = document.querySelector("#generate");
+var get = document.querySelector("#generate")
 
 get.addEventListener("click", function () {
-    passg = generatePassword();
-    document.getElementById("password").placeholder = passg;
-});
+    passg = generatePassword()
+    document.getElementById("password").placeholder = passg
+})
 
 // Start function to generate password
 function generatePassword() {
-    enter = parseInt(prompt("Please specify number of characters from 8 to 128."));
+    enter = parseInt(prompt("Please specify number of characters from 8 to 128."))
     if (!enter) {
-        alert("You must choose a number!");
+        alert("You must choose a number!")
     } else if (enter < 8 || enter > 128) {
         // Start user input prompts
-        enter = parseInt(prompt("You must choose between 8 and 128"));
-
+        enter = parseInt(prompt("You must choose between 8 and 128"))
     } else {
         // user input is validated
-        confirmNumber = confirm("Will this contain numbers?");
-        confirmCharacter = confirm("Will this contain special characters?");
-        confirmUppercase = confirm("Will this contain Uppercase letters?");
-        confirmLowercase = confirm("Will this contain Lowercase letters?");
-    };
+        confirmNumber = confirm("Will this contain numbers?")
+        confirmCharacter = confirm("Will this contain special characters?")
+        confirmUppercase = confirm("Will this contain Uppercase letters?")
+        confirmLowercase = confirm("Will this contain Lowercase letters?")
+    }
 
     // else if for 4 negative options
     if (!confirmCharacter && !confirmNumber && !confirmUppercase && !confirmLowercase) {
-        choices = alert("You must choose a criteria!");
-
+        choices = alert("You must choose a criteria!")
     }
     // First if statement that uses user input prompts to determine choices
     // Else if for 4 positive options
     else if (confirmCharacter && confirmNumber && confirmUppercase && confirmLowercase) {
-
-        choices = character.concat(number, lowercase, uppercase);
+        choices = character.concat(number, lowercase, uppercase)
     }
     // Else if for 3 positive options
     else if (confirmCharacter && confirmNumber && confirmUppercase) {
-        choices = character.concat(number, uppercase);
+        choices = character.concat(number, uppercase)
+    } else if (confirmCharacter && confirmNumber && confirmLowercase) {
+        choices = character.concat(number, lowercase)
+    } else if (confirmCharacter && confirmLowercase && confirmUppercase) {
+        choices = character.concat(lowercase, uppercase)
+    } else if (confirmNumber && confirmLowercase && confirmUppercase) {
+        choices = number.concat(lowercase, uppercase)
     }
-    else if (confirmCharacter && confirmNumber && confirmLowercase) {
-        choices = character.concat(number, lowercase);
-    }
-    else if (confirmCharacter && confirmLowercase && confirmUppercase) {
-        choices = character.concat(lowercase, uppercase);
-    }
-    else if (confirmNumber && confirmLowercase && confirmUppercase) {
-        choices = number.concat(lowercase, uppercase);
-    }
-    // Else if for 2 positive options 
+    // Else if for 2 positive options
     else if (confirmCharacter && confirmNumber) {
-        choices = character.concat(number);
-
+        choices = character.concat(number)
     } else if (confirmCharacter && confirmLowercase) {
-        choices = character.concat(lowercase);
-
+        choices = character.concat(lowercase)
     } else if (confirmCharacter && confirmUppercase) {
-        choices = character.concat(uppercase);
-    }
-    else if (confirmLowercase && confirmNumber) {
-        choices = lowercase.concat(number);
-
+        choices = character.concat(uppercase)
+    } else if (confirmLowercase && confirmNumber) {
+        choices = lowercase.concat(number)
     } else if (confirmLowercase && confirmUppercase) {
-        choices = lowercase.concat(uppercase);
-
+        choices = lowercase.concat(uppercase)
     } else if (confirmNumber && confirmUppercase) {
-        choices = number.concat(uppercase);
+        choices = number.concat(uppercase)
     }
     // Else if for 1 positive option
     else if (confirmCharacter) {
-        choices = character;
+        choices = character
+    } else if (confirmNumber) {
+        choices = number
+    } else if (confirmLowercase) {
+        choices = lowercase
     }
-    else if (confirmNumber) {
-        choices = number;
-    }
-    else if (confirmLowercase) {
-        choices = lowercase;
-    };
 
     // password variable is an array placeholder for user generated amount of length
-    var password = [];
+    var password = []
 
-    // Random selection for all variables: 
+    // Random selection for all variables:
     for (var i = 0; i < enter; i++) {
-        var pickChoices = choices[Math.floor(Math.random() * choices.length)];
-        password.push(pickChoices);
+        var pickChoices = choices[Math.floor(Math.random() * choices.length)]
+        password.push(pickChoices)
     }
-    var passg = password.join("");
-    UserInput(passg);
-    return passg;
+    var passg = password.join("")
+    UserInput(passg)
+    return passg
 }
 function UserInput(passg) {
-    document.getElementById("password").textContent = passg;
-
+    document.getElementById("password").textContent = passg
 }
